af83230b67f55b5d48f33553de075fbded5251fc
MB-study <MB-study@192.168.1.131>
2020-08-13 13:29:54 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d03/fastfood/Breast.java
+++ Java0811/src/com/pcschool/ocp/d03/fastfood/Breast.java
@@ -1,17 +1,19 @@
 package com.pcschool.ocp.d03.fastfood;
 
 public class Breast {
-    private int price = 35;
-    private static int amount = 10;
-    
-    public int getPrice() {
-        return price;
-    }
-    public Breast() {
-    	amount--;
-    }
+	private int price = 35;
+	private static int amount = 100;
+
+	public Breast() {
+		amount--;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public static int getAmount() {
 		return amount;
 	}
-    
+
 }
--- Java0811/src/com/pcschool/ocp/d03/fastfood/Leg.java
+++ Java0811/src/com/pcschool/ocp/d03/fastfood/Leg.java
@@ -1,15 +1,17 @@
 package com.pcschool.ocp.d03.fastfood;
 
 public class Leg {
-    private int price = 45;
-    private static int amount = 10;
-    
-    public int getPrice() {
-        return price;
-    }
-    public Leg() {
-    	amount--;
-    }
+	private int price = 45;
+	private static int amount = 100;
+
+	public Leg() {
+		amount--;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public static int getAmount() {
 		return amount;
 	}
