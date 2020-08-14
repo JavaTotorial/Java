b7d534822f2d9aafdd1ec0674f27209363d1da91
MB-study <MB-study@192.168.1.131>
2020-08-14 09:39:31 +0800

    Java

---
+++ Java0811/src/com/pcschool/ocp/d04/ConstrustorDemo.java
@@ -0,0 +1,8 @@
+package com.pcschool.ocp.d04;
+
+public class ConstrustorDemo {
+	public static void main(String[] args) {
+		Employee emp = new Employee("Victor", 40000);
+		System.out.println(emp);
+	}
+}
