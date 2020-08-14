33528c5b171f79bb6a43608b0cc15821fc9bc7d8
MB-study <MB-study@192.168.1.131>
2020-08-14 11:23:51 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d04/BMI.java
+++ Java0811/src/com/pcschool/ocp/d04/BMI.java
@@ -3,26 +3,23 @@ package com.pcschool.ocp.d04;
 public class BMI {
 	private double h = 170; // 100 ~ 250 (default: 170)
 	private double w = 60; // 30  ~ 150 (default: 60)
-	private double bmi;
 	
 	public BMI(double h, double w) {
-		if(h >= 100 && h <= 250)
-			this.h = h;
+		this(h);
 		if(w >= 30 && w <= 150)
 			this.w = w;
-		bmi = Double.parseDouble(String.format("%.2f", (this.w / Math.pow(this.h/100, 2))));
 	}
 
 	public BMI() {
-		this(170, 60);
 	}
 
 	public BMI(double h) {
-		this(h, 60);
+		if(h >= 100 && h <= 250)
+			this.h = h;
 	}
 
 	@Override
 	public String toString() {
-		return "BMI [h=" + h + ", w=" + w + ", bmi=" + bmi + "]";
+		return "BMI [h=" + h + ", w=" + w + "]";
 	}
 }
