ddf9da80e04b9c095b83a0894b3205cd1bfc81f8
MB-study <MB-study@192.168.1.131>
2020-08-14 15:00:47 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d05/Zoo.java
+++ Java0811/src/com/pcschool/ocp/d05/Zoo.java
@@ -1,19 +1,21 @@
 package com.pcschool.ocp.d05;
 
 class Animal {
-	public Animal() {
-		System.out.println("執行 Animal 建構子");
+	public Animal(int legs) {
+		System.out.println("執行   Animal 建構子");
+		System.out.printf("有   %d 隻腳\n" , legs);
 	}
 }
 
 class Dog extends Animal {
-	public Dog() {
-		System.out.println("執行 Dog 建構子");
+	public Dog(int legs) {
+		super(legs);
+		System.out.println("執行   Dog 建構子");
 	}
 }
 
 public class Zoo {
 	public static void main(String[] args) {
-		Dog dog = new Dog();
+		Dog dog = new Dog(4);
 	}
 }
