96b394f2050e949d842a98c9990f57b56b6cb4b8
MB-study <MB-study@192.168.1.131>
2020-08-17 13:57:12 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d06/case3/drink/Drink.java
+++ Java0811/src/com/pcschool/ocp/d06/case3/drink/Drink.java
@@ -1,11 +1,24 @@
 package com.pcschool.ocp.d06.case3.drink;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.pcschool.ocp.d06.case3.meterial.Meterial;
 
 public class Drink {
 
-	private Meterial[] meterial;
-
+	private ArrayList<Meterial> meterials;
 	private int price;
+	
+	public Drink() {
+		meterials = new ArrayList<>();
+	}
+	
+	public void addMeterial(Meterial... meterial) {
+		Arrays.stream(meterial).forEach(m -> meterials.add(m));
+	}
 
+	public int getPrice() {
+		return meterials.stream().mapToInt(meterial -> meterial.getPrice()).sum();
+	}
 }
--- Java0811/src/com/pcschool/ocp/d06/case3/drink/PearlTea.java
+++ Java0811/src/com/pcschool/ocp/d06/case3/drink/PearlTea.java
@@ -1,5 +1,5 @@
 package com.pcschool.ocp.d06.case3.drink;
 
 public class PearlTea extends Drink {
-
+	
 }
