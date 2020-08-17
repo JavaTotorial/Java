614643079f925718f77329685c06b434fed49ee1
MB-study <MB-study@192.168.1.131>
2020-08-17 11:00:20 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d06/case2/Company.java
+++ Java0811/src/com/pcschool/ocp/d06/case2/Company.java
@@ -1,5 +1,8 @@
 package com.pcschool.ocp.d06.case2;
 
+import com.pcschool.ocp.d06.case2.employee.Manager;
+import com.pcschool.ocp.d06.case2.employee.Sales;
+
 public class Company {
 	public static void main(String[] args) {
 		Manager manager = new Manager();
