d8280347401ea2884018e72bc40eade9fce5fceb
MB-study <MB-study@192.168.1.131>
2020-08-18 15:07:10 +0800

    Java

--- Java0811/src/com/pcschool/ocp/d07/case2/Zoo2.java
+++ Java0811/src/com/pcschool/ocp/d07/case2/Zoo2.java
@@ -5,6 +5,10 @@ public class Zoo2 {
 		Animal animal1 = new Fish();
 		Animal animal2 = new Penguin();
 		
-		
+		if(animal1 instanceof Bird)
+			((Bird) animal1).move();
+		else
+			System.out.println("Fish　不可轉型成　Bird");
+		((Bird) animal2).move();
 	}
 }
