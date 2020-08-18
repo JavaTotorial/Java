e079b6005beadf49aa376e0414eb33fad5e34642
MB-study <MB-study@192.168.1.131>
2020-08-18 11:37:10 +0800

    Java

---
+++ Java0811/src/com/pcschool/ocp/d07/case1_2/food/Food.java
@@ -0,0 +1,46 @@
+package com.pcschool.ocp.d07.case1_2.food;
+
+import com.pcschool.ocp.d07.case1.material.Material;
+import java.util.Arrays;
+
+public class Food {
+
+	private String name;
+
+	private Material[] materials;
+
+	public Food() {
+	}
+
+	public Food(String name) {
+		setName(name);
+	}
+
+	public int getPrice() {
+		return Arrays.stream(materials).mapToInt(m -> m.getPrice()).sum();
+	}
+
+	public Food addMaterial(Material material) {
+		Material[] new_materials = Arrays.copyOf(materials, materials.length + 1);
+		new_materials[new_materials.length - 1] = material;
+		setMaterials(new_materials);
+		return this;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Material[] getMaterials() {
+		return materials;
+	}
+
+	public void setMaterials(Material[] materials) {
+		this.materials = materials;
+	}
+
+}
