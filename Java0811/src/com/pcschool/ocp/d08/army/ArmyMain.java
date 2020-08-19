e2749d25b64a34acd9f23f643147ae84c8e0c02b
MB-study <MB-study@192.168.1.131>
2020-08-19 11:37:04 +0800

    Java

---
+++ Java0811/src/com/pcschool/ocp/d08/army/ArmyMain.java
@@ -0,0 +1,20 @@
+package com.pcschool.ocp.d08.army;
+
+import java.util.Arrays;
+
+public class ArmyMain {
+	public static void main(String[] args) {
+		Airplane[] airplanes = { new Fighter(), new Fighter(), new Fighter() };
+		Car[] cars = { new Tank(), new Truck(), new Truck(), new Tank() };
+		Weapon[] weapons = { new Fighter(), new Tank() };
+		
+		System.out.println("================");
+		
+		// cars 要統一發射武器
+		Arrays.stream(cars).filter(car -> car instanceof Weapon).forEach(c -> ((Weapon)c).shoot());
+
+		// weapons 要統一發射武器
+		Arrays.stream(weapons).forEach(Weapon::shoot);
+
+	}
+}
